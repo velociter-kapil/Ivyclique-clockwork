8614c18b2d40c86cf2ef0992e8b2fdc97400d85c
sharepointapp <ravi@velociters.com>
2016-03-03 18:02:08 +0530

    comentsd

---
+++ test_file.php
@@ -0,0 +1,15 @@
+git@github.com:sharepointapp/test.git
+…or create a new repository on the command line
+
+
+echo "# test" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin git@github.com:sharepointapp/test.git
+git push -u origin master
+…or push an existing repository from the command line
+
+
+git remote add origin git@github.com:sharepointapp/test.git
+git push -u origin master
